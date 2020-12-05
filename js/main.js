ab211f336d7c1c9e9b8282f7e9b0bc9d5585524d
edstarck <webeduard@yandex.ru>
2020-12-05 22:33:49 +0300

    add animation for images in header

--- js/main.js
+++ js/main.js
@@ -45,12 +45,63 @@ function initNavigation() {
     },
     onEnter: ({ direction }) => navAnimation(direction),
     onLeaveBack: ({ direction }) => navAnimation(direction),
-    markers: true,
+    // markers: true,
   });
+}
+
+function moveImages(e) {
+  const { offsetX, offsetY, target } = e;
+  const { clientWidth, clientHeight } = target;
+
+  // console.log(offsetX, offsetY, clientWidth, clientHeight);
+  // get 0 0 in the center
+  const xPos = offsetX / clientWidth - 0.5;
+  const yPos = offsetY / clientHeight - 0.5;
+
+  const leftImages = gsap.utils.toArray('.hg__left .hg__image');
+  const rightImages = gsap.utils.toArray('.hg__right .hg__image');
+
+  const modifier = (index) => (!index ? 0.5 : index * 1.2 + 0.5);
+
+  // move left 3 images
+  leftImages.forEach((img, index) => {
+    gsap.to(img, {
+      duration: 1.2,
+      x: xPos * 20 * modifier(index),
+      y: yPos * 30 * modifier(index),
+      rotationY: xPos * 40,
+      rotationX: yPos * 10,
+      ease: 'Power3.out',
+    });
+  });
+
+  // move right 3 images
+  rightImages.forEach((img, index) => {
+    gsap.to(img, {
+      duration: 1.2,
+      x: xPos * 20 * modifier(index),
+      y: yPos * 30 * modifier(index),
+      rotationY: xPos * 40,
+      rotationX: -yPos * 10,
+      ease: 'Power3.out',
+    });
+  });
+
+  gsap.to('.decor__circle', {
+    duration: 1.7,
+    x: xPos * 100,
+    y: yPos * 120,
+    ease: 'Power4.out',
+  });
+}
+
+function initHeaderTilt() {
+  document.querySelector('header').addEventListener('mousemove', moveImages);
 }
 
 function init() {
   initNavigation();
+  initHeaderTilt();
 }
 
 window.addEventListener('load', function () {
